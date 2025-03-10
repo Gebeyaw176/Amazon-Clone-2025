354fa0c13a4a5dbe32a2476567cb3a5b783d454d
Gebeyaw176 <gebeyawyesigat349@gmail.com>
2025-03-09 21:14:25 -0400

    deploy

--- src/Components/Category/Category.jsx
+++ src/Components/Category/Category.jsx
@@ -1,25 +1,15 @@
-import React from 'react'
-import CategoryCard from './CategoryCard'
-import { categoryInfos } from './categoryFullInfos'
-import classes from './Category.module.css'
+import React from "react";
+import CategoryCard from "./CategoryCard";
+import {categoryInfos} from "./categoryFullinfos"
+import classes from "./Category.module.css";
 function Category() {
   return (
-    <section
-        className={classes.category_container}>
-{
-categoryInfos.map((infos,i)=>(
-    <CategoryCard data = {infos} key={i}/>
-))
-}
-   
+    <section className={classes.category_container}>
+      {categoryInfos.map((infos, i) => (
+        <CategoryCard data={infos} key={i} />
+      ))}
     </section>
-   
-  )
+  );
 }
 
-export default Category
-
-
-
-
-
+export default Category;
