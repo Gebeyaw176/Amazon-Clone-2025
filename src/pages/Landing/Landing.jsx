466ecf3139790d0bbb51ab9f1a58f2343fcce2b6
Gebeyaw176 <gebeyawyesigat349@gmail.com>
2025-03-11 23:57:48 -0400

    adding footer

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -3,6 +3,7 @@ import LayOut from "../../Components/LayOut/LayOut";
 import Category from "../../Components/Category/Category";
 import Product from "../../Components/Product/Product";
 import CarouselEffect from "../../Components/Carousel/Carousel";
+import Footer from "../../Components/Footer/Footer";
 
 function Landing() {
   return (
@@ -10,6 +11,7 @@ function Landing() {
       <CarouselEffect />
       <Category />
       <Product />
+      <Footer/>
     </LayOut>
   );
 }
